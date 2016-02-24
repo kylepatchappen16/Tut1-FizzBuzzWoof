f67cb3418b94d7d85046e3423e7f17ca6126004f
kylepatchappen16 <211523417@stu.ukzn.ac.za>
2016-02-24 15:06:31 +0200

    FizzBuzzWoof

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,18 @@ using namespace std;
 int main()
 {
 	int i;
-	for ( i = 1; i <= 20; i++)
+	for ( i = 1; i <= 50; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			cout << "FizzBuzz" << endl;
+		if (i % 3 == 0 && i % 5 == 0 && 1 % 7 == 0)
+			cout << "FizzBuzzWoof" << endl;
 		else
 			if (i % 3 == 0)
 				cout << "Fizz" << endl;
 			else
 				if (i % 5 == 0)
 					cout << "Buzz" << endl;
+				else if (i % 7 == 0)
+					cout << "Woof" << endl;
 				else cout << i << endl;
 	}
 	return 0;
